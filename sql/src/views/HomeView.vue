9c387d78aea1bc2e8e5c94d8f555a936a44d9979
johnnylu3 <lujohnny517@gmail.com>
2023-06-04 12:03:42 -0400

    Start

--- sql/src/views/HomeView.vue
+++ sql/src/views/HomeView.vue
@@ -1,17 +1,11 @@
 <template>
-    <div>
+  <div>
 
-    </div>
+  </div>
 </template>
 
-<script>
-import { createClient } from '@supabase/supabase-js'
-const supabase = createClient('https://ctlzmvtybiilsnqpktpq.supabase.co/', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bHptdnR5YmlpbHNucXBrdHBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM2ODg4OTIsImV4cCI6MTk5OTI2NDg5Mn0.Vrluan5meDqRU07ZzsOYd_Hwe54PR6FwlvUNYJhUtTM')
-const { data, error } = await supabase
-  .from('Details')
-  .select('*')
+<script setup>
 
-  console.log(data)
 </script>
 
 <style lang="scss" scoped>
