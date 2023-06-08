c15913841a2b754f73aeed434adda1e8df95edba
Ucheong <ucheongsou@gmail.com>
2023-06-08 17:59:55 -0400

    fixed it a bit

--- sql/src/views/LoginView.vue
+++ sql/src/views/LoginView.vue
@@ -1,18 +1,28 @@
 <template>
-  <a><button class="back">Back</button></a>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+  />
+  <a
+    ><button class="back"><i class="fa fa-home"></i></button
+  ></a>
   <h1>Login</h1>
-  <div class="loginForm">
-    <div class="email">
-      <input type="email" placeholder="Email" id="email" />
+  <form>
+    <div class="loginForm">
+      <div class="email">
+        <input type="email" placeholder="Email" id="email" />
+      </div>
+      <div class="password">
+        <input type="password" placeholder="Password" id="password" />
+      </div>
+      <input type="submit" value="Log In" id="login" @click="Login" />
     </div>
-    <div class="password">
-      <input type="password" placeholder="Password" id="password" />
-    </div>
-    <input type="submit" value="Log In" id="login" @click="Login" />
-  </div>
+  </form>
   <div class="toSignUp">
     Don't have an account?
-    <a><RouterLink to="/signUp">Sign Up Here</RouterLink></a>
+    <a><RouterLink to="/signup">Sign Up Here</RouterLink></a>
   </div>
 </template>
 
