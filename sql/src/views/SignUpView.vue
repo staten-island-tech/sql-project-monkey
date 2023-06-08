d0086073be02ddfd8530736e410ded3ce72ed454
Ucheong <ucheongsou@gmail.com>
2023-06-08 17:56:43 -0400

    idk what happened

---
+++ sql/src/views/SignUpView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="signUp">
+    <h1>Sign Up</h1>
+    <form class="signUpForm">
+      <h2 class="text">Email</h2>
+      <div class="email">
+        <input type="email" id="email" />
+      </div>
+      <h2 class="text">Password</h2>
+      <div class="password">
+        <input type="password" placeholder="At least 6 characters" id="password" />
+      </div>
+      <h2 class="text">Re-enter Password</h2>
+      <div class="password">
+        <input type="password" id="password-confirm" />
+      </div>
+
+      <input type="submit" value="Sign Up" id="signup" @click="signup" />
+    </form>
+    <div class="login">
+      Already have an account? <a><RouterLink to="/login">Login</RouterLink></a>
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
