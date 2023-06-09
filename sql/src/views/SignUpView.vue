d58218e444c95564b1c2006bea97f322f2a14235
Ucheong <ucheongsou@gmail.com>
2023-06-09 03:25:02 -0400

    home button added

--- sql/src/views/SignUpView.vue
+++ sql/src/views/SignUpView.vue
@@ -1,4 +1,14 @@
 <template>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+  />
+  <a
+    ><RouterLink to="/home"
+      ><button class="back"><i class="fa fa-home"></i></button></RouterLink
+  ></a>
   <div class="signUp">
     <h1 class="title">Sign Up</h1>
     <form class="signUpForm">
