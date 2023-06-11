084004245bba24dd80312233b43386547152cebb
Ucheong <ucheongsou@gmail.com>
2023-06-11 19:44:30 -0400

    delete

--- sql/src/views/LoginView.vue
+++ sql/src/views/LoginView.vue
@@ -63,9 +63,7 @@ export default {
       } else {
         signIn(supabase, userEmail, userPassword)
       }
-      this.$emit('loggedin')
-    },
-    emits: ['loggedin']
+    }
   }
 }
 </script>
